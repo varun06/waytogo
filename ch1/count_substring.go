36e66edfff95a58df37b5a4c6356d1e513401469
Varun <varunkuamr.saini06@gmail.com>
2015-03-07 07:47:26 -0600

    new progs on strings

---
+++ ch1/count_substring.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string = "Hello, how is it going, Hugo?"
+	var manyG = "gggggggg"
+	fmt.Printf("Number of H's in the %s is: ", str)
+	fmt.Printf("%d\n", strings.Count(str, "H"))
+	fmt.Printf("Number of double g's in %s is: ", manyG)
+	fmt.Printf("%d\n", strings.Count(manyG, "gg"))
+}
